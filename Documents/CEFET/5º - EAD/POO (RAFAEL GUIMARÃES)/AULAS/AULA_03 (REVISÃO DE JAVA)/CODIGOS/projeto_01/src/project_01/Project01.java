2b6f64662d91b3540abd82a875e58a9be2315839
BMB <giovannioliveira5@gmail.com>
2021-08-19 18:53:32 -0300

    Jason

---
+++ Documents/CEFET/5º - EAD/POO (RAFAEL GUIMARÃES)/AULAS/AULA_03 (REVISÃO DE JAVA)/CODIGOS/projeto_01/src/project_01/Project01.java
@@ -0,0 +1,14 @@
+package project_01;
+public class Project01{
+	
+	public static void main (String[] args) {
+		System.out.println("Olá mundo. ");
+		int idade = 20;
+		int n;
+		n = 20;
+		n= 25*2;
+		System.out.println(idade);
+		System.out.println(n);
+		
+	}
+}
