78efb881c77dde0db8e064cc60a6ff99c5c00869
Giovanni Madrignani <87835741+madrignani@users.noreply.github.com>
2024-06-18 15:54:07 -0300

    Delete fund.web/lab01/ExercicioJavaScript04_Script.js

--- fund.web/lab01/ExercicioJavaScript04_Script.js
+++
@@ -1,8 +0,0 @@
-fib = function(numero){
-    for(var sequencia = [0,1], i=0,j=1,k=0 ; k<numero ; i=j,j=x,k++){
-        x=i+j;
-        sequencia.push(x);
-    }
-    document.write(sequencia);
-}
-fib(100)
